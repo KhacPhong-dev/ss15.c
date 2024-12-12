3ff6f48848bb59be011a043c8f65f42ef1fd8e32
KhacPhong-dev <kaito0676@gmail.com>
2024-12-12 15:04:17 +0700

    Add files via upload

--- b8.c
+++ b8.c
@@ -1,20 +1,17 @@
+// Khai báo và gán giá trị cho 1 chuỗi bất kỳ, viết chương trình viết hoa các chữ cái đầu của các từ trong chuỗi đó.
+
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 int main() {
     char chuoi[] = "hello world";
-    int len = strlen(chuoi);
-    if (chuoi[0] >= 'a' && chuoi[0] <= 'z') {
-        chuoi[0] -= 32;
-    }
-    for (int i = 1; i < len; i++) {
-        if (chuoi[i] == ' ' && i + 1 < len) {
-            if (chuoi[i + 1] >= 'a' && chuoi[i + 1] <= 'z') {
-                chuoi[i + 1] -= 32;
-            }
+    chuoi[0] = toupper(chuoi[0]);
+    for (int i = 1; i < strlen(chuoi); i++) {
+        if (chuoi[i-1] == ' ') {
+            chuoi[i] = toupper(chuoi[i]);
         }
     }
-
     printf("%s\n", chuoi);
-
     return 0;
 }
